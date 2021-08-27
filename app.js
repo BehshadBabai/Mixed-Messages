474e0d574a54b5a3475aeebee1096c62c6ec2b22
Behshad <behshad.babai@gmail.com>
2021-08-27 16:53:59 -0700

    app.js file displaying random messages on each run

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
+// Author: Behshad Babai
 
-console.log('Hello world');
+// array containing random messages
+let msg = ['Believe in yourself. Your limitation—it\'s only your imagination.', 'Work hard. Push yourself, because no one else is going to do it for you.', 'Don\'t wait.', 'Step outside your comfort zone.', 'Be a dreamer and a doer'];
+
+// function that generates random msg
+const generateRandomMsg = () => {
+    return msg[(Math.floor(Math.random()*6))];
+};
+
+// displaying the random message
+console.log(generateRandomMsg());
